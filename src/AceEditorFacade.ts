c02d71fa46081ed44a3666e2acac6034166f97d9
streamich <streamich@gmail.com>
2023-12-03 15:24:49 +0100

    feat: 🎸 add event listener

--- src/AceEditorFacade.ts
+++ src/AceEditorFacade.ts
@@ -5,14 +5,38 @@ export class AceEditorFacade implements EditorFacade {
   public onchange?: (changes: SimpleChange[] | void) => void;
   public onselection?: () => void;
 
-  constructor(protected readonly editor: Ace.Editor) {}
+  constructor(protected readonly editor: Ace.Editor) {
+    editor.on('change', this.onChange);
+  }
+
+  private readonly onChange = (delta: Ace.Delta) => {
+    if (delta && typeof delta == 'object' && delta.start && delta.end) {
+      const doc = this.editor.getSession().getDocument();
+      const start = doc.positionToIndex(delta.start);
+      const text = delta.lines.join('\n');
+      switch (delta.action) {
+        case 'insert':
+          // console.log([start, 0, text]);
+          this.onchange?.([[start, 0, text]]);
+          break;
+        case 'remove':
+          // console.log([start, text.length, '']);
+          this.onchange?.([[start, text.length, '']]);
+          break;
+        default:
+          this.onchange?.();
+      }
+    } else this.onchange?.();
+  };
 
   public get(): string {
     return this.editor.getValue();
   }
 
+  // If there is no dedicated method for getting the length of the text, then
+  // better not to implement this method at all.
   // public getLength(): number {
-  //   throw new Error('Method not implemented.');
+  //   return this.editor.getValue().length;
   // }
 
   public set(text: string): void {
@@ -35,7 +59,7 @@ export class AceEditorFacade implements EditorFacade {
   //   throw new Error('Method not implemented.');
   // }
 
-  // public dispose(): void {
-  //   throw new Error('Method not implemented.');
-  // }
+  public dispose(): void {
+    this.editor.off('change', this.onChange);
+  }
 }
