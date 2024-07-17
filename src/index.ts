78364c3f17e1c7c9862dbf7dfdc9c2f495a82062
Vadim Dalecky <streamich@gmail.com>
2024-07-18 01:24:53 +0200

    feat: 🎸 update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {StrBinding} from 'collaborative-editor';
 import {AceEditorFacade} from './AceEditorFacade';
 import type {Ace} from 'ace-builds';
-import type {StrApi} from 'json-joy/es2020/json-crdt';
+import type {StrApi} from 'json-joy/lib/json-crdt';
 
 export const bind = (str: StrApi, editor: Ace.Editor, polling?: boolean): (() => void) =>
   StrBinding.bind(str, new AceEditorFacade(editor), polling);
